005648d590f2dd32139fa107a7d1a471904b5e00
nikhil47nick <nikhiljoji94@gmail.com>
2023-01-02 14:29:21 +0530

    Added : TicTacToe

--- src/com/bridgelabz/Day7/TicTacToe.java
+++ src/com/bridgelabz/Day7/TicTacToe.java
@@ -63,28 +63,28 @@ public class TicTacToe {
     }
 
     public char result(){
-        if (board[1]==board[2] && board[2]==board[3]){
+        if (board[1]==board[2] && board[2]==board[3] && board[1] !=' '){
             win = board[1];
             return 1;
-        }else if (board[4]==board[5] && board[5]==board[6]){
+        }else if (board[4]==board[5] && board[5]==board[6]&& board[4] !=' '){
             win = board[4];
             return 1;
-        }else if (board[7]==board[8] && board[8]==board[9]){
+        }else if (board[7]==board[8] && board[8]==board[9]&& board[7] !=' '){
             win = board[7];
             return 1;
-        }else if (board[1]==board[4] && board[4]==board[7]){
+        }else if (board[1]==board[4] && board[4]==board[7]&& board[1] !=' '){
             win = board[1];
             return 1;
-        }else if (board[2]==board[5] && board[5]==board[8]){
+        }else if (board[2]==board[5] && board[5]==board[8]&& board[2] !=' '){
             win = board[2];
             return 1;
-        }else if (board[3]==board[6] && board[6]==board[9]){
+        }else if (board[3]==board[6] && board[6]==board[9]&& board[3] !=' '){
             win = board[3];
             return 1;
-        }else if (board[1]==board[5] && board[5]==board[9]){
+        }else if (board[1]==board[5] && board[5]==board[9]&& board[1] !=' '){
             win = board[1];
             return 1;
-        }else if (board[3]==board[5] && board[5]==board[7]){
+        }else if (board[3]==board[5] && board[5]==board[7]&& board[3] !=' '){
             win = board[3];
             return 1;
         }else{
@@ -100,23 +100,23 @@ public class TicTacToe {
 
     public void computerPlayer(char ch){
 
-            if(board[2]==board[3] || board[4] ==board[7] || board[5]==board[9] && board[1] == ' '){
+            if(board[1] == ' ' && board[2]==board[3] || board[4] ==board[7] || board[5]==board[9] ){
                     board[1]=ch;
-            }else if(board[1]==board[3] || board[5] ==board[8] && board[2] == ' '){
+            }else if(board[2] == ' ' && board[1]==board[3] || board[5] ==board[8] ){
                     board[2]=ch;
-            }else if(board[1]==board[2] || board[6] ==board[9] || board[5]==board[7] && board[3] == ' '){
+            }else if(board[3] == ' ' && board[1]==board[2] || board[6] ==board[9] || board[5]==board[7] ){
                     board[3]=ch;
-            }else if(board[1]==board[7] || board[5] ==board[6] && board[4] == ' '){
+            }else if(board[4] == ' ' && board[1]==board[7] || board[5] ==board[6]){
                     board[4] = ch;
-            }else if(board[1]==board[9] || board[4] ==board[6] || board[2]==board[8] || board[3]==board[7] && board[5] == ' '){
+            }else if(board[5] == ' ' && board[1]==board[9] || board[4] ==board[6] || board[2]==board[8] || board[3]==board[7]){
                         board[5]=ch;
-            }else if(board[3]==board[9] || board[4] ==board[5] && board[6] == ' '){
+            }else if(board[6] == ' ' && board[3]==board[9] || board[4] ==board[5] ){
                         board[6]=ch;
-            }else if(board[1]==board[4] || board[8] ==board[9] || board[5]==board[3]&& board[7] == ' '){
+            }else if(board[7] == ' ' && board[1]==board[4] || board[8] ==board[9] || board[5]==board[3]){
                         board[7]=ch;
-            }else if(board[2]==board[5] || board[7] ==board[9] && board[8] == ' '){
+            }else if(board[8] == ' ' && board[2]==board[5] || board[7] ==board[9] ){
                         board[8]=ch;
-            }else if(board[3]==board[6] || board[7] ==board[8] || board[1]==board[5] && board[9] == ' '){
+            }else if(board[9] == ' ' && board[3]==board[6] || board[7] ==board[8] || board[1]==board[5] ){
                         board[9]=ch;
             }else if (board[1] == ' '){
                     board[1]=ch;
