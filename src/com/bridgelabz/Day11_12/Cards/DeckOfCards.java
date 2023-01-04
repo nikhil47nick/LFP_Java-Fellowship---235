750adb0f10696d3a0b1d6709a3dd6eddb2f84b7f
nikhil47nick <nikhiljoji94@gmail.com>
2023-01-04 19:17:22 +0530

    Added : Day11 program

--- src/com/bridgelabz/Day11_12/Cards/DeckOfCards.java
+++ src/com/bridgelabz/Day11_12/Cards/DeckOfCards.java
@@ -1,36 +1,51 @@
 package com.bridgelabz.Day11_12.Cards;
 
+
+import javax.swing.plaf.IconUIResource;
 import java.util.Random;
 
 public class DeckOfCards {
-   String[][] deck;
-
-
-
+   String[][] cards;
+   String deck[]= new String[52];
 
     DeckOfCards(){
-        deck =new String[][] {{"Clubs", "Diamonds", "Hearts", "Spades"},{"2", "3", "4", "5", "6", "7", "8","9", "10", "Jack", "Queen", "King", "Ace"}};
+        cards =new String[][] {{"Clubs", "Diamonds", "Hearts", "Spades"},{"2", "3", "4", "5", "6", "7", "8","9", "10", "Jack", "Queen", "King", "Ace"}};
 
     }
-
     public void shuffle(){
-
-
-        for(int i = 0; i < deck.length; i++) {
-            for(int j = 0; j < deck[i].length; j++) {
-
-                int cardLucky = (int)(Math.random()*13) + 1;
-                int suitLucky = (int)(Math.random()*4) + 1;
-
-                System.out.print(deck[cardLucky][suitLucky]);
-
+        int index;
+        for(int i = 0; i < 4; i++) {
+            for(int j = 0; j < 13; j++) {
+                 do{
+                     Random ran = new Random();
+                    index  = ran.nextInt(52);
+                }while(deck[index] != null);
+                deck[index] =  cards[1][j] + cards[0][i];
             }
         }
     }
 
+public void display(String[] player){
+    for(int i = 0; i < player.length; i++) {
+            System.out.println(player[i]);
+        }
+}
 
 
+    public String[] playerCard() {
 
+        int index;
+        String [] player = new String[9];
+        for(int i = 0; i < 9; i++) {
 
+            do{
+                Random ran = new Random();
+                index  = ran.nextInt(52);
+            }while(deck[index] == null);
+            player[i] = deck[index];
+            deck[index]=null;
+        }
+        return player;
+    }
 }
 
--- src/com/bridgelabz/Day11_12/Cards/Main.java
+++ src/com/bridgelabz/Day11_12/Cards/Main.java
@@ -2,6 +2,9 @@ package com.bridgelabz.Day11_12.Cards;
 
 public class Main {
 
+
+
+
     public static void main(String[] args) {
         String[] player1= new String[9];
         String[] player2= new String[9];
@@ -10,6 +13,15 @@ public class Main {
 
         DeckOfCards cd = new DeckOfCards();
         cd.shuffle();
+        cd.display(cd.deck);
+        player1 = cd.playerCard();
+        player2 = cd.playerCard();
+        player3 = cd.playerCard();
+        player4 = cd.playerCard();
+        System.out.println("Player 1 cards");cd.display(player1);
+        System.out.println("Player 2 cards");cd.display(player2);
+        System.out.println("Player 3 cards");cd.display(player3);
+        System.out.println("Player 4 cards");cd.display(player4);
 
     }
 
